6e99c856dfa2e0d1e54433adb0c851e94866aa02
Otman Bouftoug <obouftou@e2r7p4.1337.ma>
2025-02-06 22:13:30 +0100

    server_start

--- server.c
+++ server.c
@@ -6,7 +6,20 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 22:27:30 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/06 16:59:22 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/06 22:13:03 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int main(int ac, char **av)
+{
+    struct sigaction    sig;
+    int                 pid;
+
+    pid = getpid();
+    ft_putstr_fd(CY "Server PID: \e[0m", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putstr_fd("\n" GRN "Waiting...\n" "\e[0m", 1);
+    
+}
