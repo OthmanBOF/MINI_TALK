34480d098dd1f6eab2b7f173b427d79cb65e0a4b
Otman Bouftoug <obouftou@e2r7p4.1337.ma>
2025-02-06 14:53:48 +0100

    val_pid

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 22:27:20 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/06 14:37:14 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/06 14:53:27 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,29 @@
 
 
 
+int	pid_check(char *pid_str)
+{
+	int	i;
+	int	pid;
 
-
-
-
+	i = 0;
+	while (pid_str[i])
+	{
+		if (!ft_isdigit(pid_str[i]))
+		{
+			ft_putstr_fd(RED "PID has non-digit chars.\n" "\e[0m", 2);
+			return (0);
+		}
+		i++;
+	}
+	pid = ft_atoi(pid_str);
+	if (pid <= 0)
+	{
+		ft_putstr_fd(RED "PID must be positive.\n" "\e[0m", 2);
+		return (0);
+	}
+	return (pid);
+}
 
 
 int	main (int ac ,char **av)
