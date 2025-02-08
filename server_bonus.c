1587403122dd7a36224206f137cf5248371ff2af
Otman Bouftoug <obouftou@e2r1p2.1337.ma>
2025-02-08 21:45:54 +0100

    norminette

--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 19:17:34 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/08 21:40:27 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/08 21:45:20 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	var_res(int *pid, int *w_bit, char *c, int *buff_i)
 	*pid = 0;
 	*w_bit = 0;
 	*c = 0;
-	*buff_i = 0; 
+	*buff_i = 0;
 }
 
 void	print_ack_buff(char *buffer, int *buff_i, int pid)
 {
 	int	i;
-	
+
 	i = 0;
 	while (i < *buff_i)
 		ft_putchar_fd(buffer[i++], 1);
@@ -34,20 +34,20 @@ void	print_ack_buff(char *buffer, int *buff_i, int pid)
 
 void	bit_to_char(int sin, siginfo_t *info, void *context)
 {
-	static int	pid	= 0;
+	static int	pid = 0;
 	static int	w_bit = 0;
 	static char	c = 0;
-	static char buffer[4] = {0};
+	static char	buffer[4] = {0};
 	static int	buff_i = 0;
 
 	(void)context;
 	if (pid != info->si_pid)
 		var_res(&pid, &w_bit, &c, &buff_i);
 	pid = info->si_pid;
-	if (sin = SIGUSR1)
+	if (sin == SIGUSR1)
 		c |= (1 << w_bit);
 	w_bit++;
-	if (w_bit = 8)
+	if (w_bit == 8)
 	{
 		buffer[buff_i++] = c;
 		c = 0;
